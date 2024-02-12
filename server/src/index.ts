a98ec5189dd081f38c09e9ca3178aceb99c77d96
Diego Saravia <saravia__2000@hotmail.com>
2024-02-11 22:39:43 -0600

    routes configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import path from "path";
+import fs from "fs";
 
 dotenv.config();
 const app = express();
 app.use(morgan("dev"));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//
+// Loading routes
+//
+console.log("Loading routes...");
+const routesPath = path.join(__dirname, "routes");
+Promise.all(
+  fs.readdirSync(routesPath).map(async (filename) => {
+    try {
+      const routeFile = path.join(routesPath, filename);
+      const routeFilename = path.parse(routeFile).name;
+      const routeUrl = routeFilename == "index" ? "" : routeFilename;
+      const route = await import(routeFile);
+      app.use(`/${routeUrl}`, route.default);
+      console.log(`Route /${routeUrl} loaded correctly`);
+    } catch (error: any) {
+      console.log(
+        `Error trying to load routes file ${filename}: ${error.message}`
+      );
+    }
+  })
+).then(() => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
