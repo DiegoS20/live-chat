641fa59ddf4ce0e96aa916524617103bc616d2d2
Diego Saravia <saravia__2000@hotmail.com>
2024-02-14 12:39:50 -0600

    adding register route

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -4,11 +4,13 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import App from "./App";
+import Register from "./pages/Register";
 
 export default createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />} />
+      <Route path="/register" element={<Register />} />
     </>
   )
 );
